92be0a0bff76009c23d490928b2d715ba9fd6939
Plops013 <pecorafatec@gmail.com>
2020-08-27 20:43:20 -0300

    fix: setting environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCtqy89josnfLzZiqtma0qsnJGNMPtF_Yo',
+    authDomain: 'fir-notification-783aa.firebaseapp.com',
+    databaseURL: 'https://fir-notification-783aa.firebaseio.com',
+    projectId: 'fir-notification-783aa',
+    storageBucket: 'fir-notification-783aa.appspot.com',
+    messagingSenderId: '58246963133',
+    appId: '1:58246963133:web:51f118e847ae1f9014fcd4',
+    measurementId: 'G-CR6QCVXKC6'
+  }
 };
