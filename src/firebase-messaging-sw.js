407a4e84c420c4a61a6298a2f031fea9dbc73250
Plops013 <pecorafatec@gmail.com>
2020-08-28 12:54:55 -0300

    ...

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -14,6 +14,14 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+  }
 messaging.onBackgroundMessage(function (payload) {
   const broadcast = new BroadcastChannel('messaging-sw');
   broadcast.postMessage(payload);
